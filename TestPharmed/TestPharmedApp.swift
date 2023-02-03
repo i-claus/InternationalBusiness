d21f7eeae44def99243d9a1db80d1592f54eddb2
Selcuk <devselcukdev@gmail.com>
2023-02-03 18:34:48 +0100

    Initial Commit

---
+++ TestPharmed/TestPharmedApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestPharmedApp.swift
+//  TestPharmed
+//
+//  Created by SELCUK YILDIZ on 03.02.23.
+//
+
+import SwiftUI
+
+@main
+struct TestPharmedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
